0fc8b547f4727e1c037b50422e0d322d30f2290c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 23:37:24 +0000

    Automatically backed up by Learn

--- src/home.js
+++ src/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}

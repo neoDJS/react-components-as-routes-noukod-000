862cd752014287d3e82831672d8ebb7b739634cd
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 23:39:52 +0000

    Automatically backed up by Learn

--- src/login.js
+++ src/login.js
@@ -4,7 +4,17 @@ export default class Login extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is my about component!</h1>
+        <form>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+            <label htmlFor="username">Username</label>
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
       </div>
     )
   }

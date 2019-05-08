afd7420e91e49c9b52e4b8e0b017df3110885827
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 23:40:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,39 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-const About = () => {
- return (
-   <div>
-     <h1>This is my about component!</h1>
-   </div>
- );
-};
-
-const Login = () => {
- return (
-   <div>
-     <form>
-       <div>
-         <input type="text" name="username" placeholder="Username" />
-         <label htmlFor="username">Username</label>
-       </div>
-       <div>
-         <input type="password" name="password" placeholder="Password" />
-         <label htmlFor="password">Password</label>
-       </div>
-       <input type="submit" value="Login" />
-     </form>
-   </div>
- );
-};
 
 ReactDOM.render((
   <Router>

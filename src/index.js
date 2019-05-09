95631cc26ed8904af12f1dd2ab5c29fa2c506336
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 00:19:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,51 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './home';
 import About from './about';
 import Login from './login';
-import Navbar from './navBar';
+// import Navbar from './navBar';
+
+
+/* Add basic styling for NavLinks */
+const link = {
+ width: '100px',
+ padding: '12px',
+ margin: '0 6px 6px',
+ background: 'blue',
+ textDecoration: 'none',
+ color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () =>
+ <div>
+   <NavLink
+     to="/"
+     /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+     exact
+     /* add styling to Navlink */
+     style={link}
+     /* add prop for activeStyle */
+     activeStyle={{
+       background: 'darkblue'
+     }}
+   >Home</NavLink>
+   <NavLink
+     to="/about"
+     exact
+     style={link}
+     activeStyle={{
+       background: 'darkblue'
+     }}
+   >About</NavLink>
+   <NavLink
+     to="/login"
+     exact
+     style={link}
+     activeStyle={{
+       background: 'darkblue'
+     }}
+   >Login</NavLink>
+ </div>;
+
 
 
 ReactDOM.render((
